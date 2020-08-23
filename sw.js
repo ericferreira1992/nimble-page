f10c14f8dda6dd75b47615683b83565868ec6b78
Eric Ferreira <eric.ferreira@socialbank.com.br>
2020-08-23 20:08:37 -0300

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('install', e => {
+	console.log(e);
+	console.log(caches);
 	e.waitUntil(
 		caches.open('app-cache').then(cache => {
+			console.log(cache);
 			return cache.addAll([
 				'/nimble-page/'
 			]);
